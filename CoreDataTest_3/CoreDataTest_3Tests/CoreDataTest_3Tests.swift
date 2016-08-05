ed4f8bb69c6cddc5ae2551cb8289c37fa1796f08
Min-ho Kim <xho1995@gmail.com>
2016-08-05 20:47:31 +0900

    Add: First Template Code

---
+++ CoreDataTest_3/CoreDataTest_3Tests/CoreDataTest_3Tests.swift
@@ -0,0 +1,36 @@
+//
+//  CoreDataTest_3Tests.swift
+//  CoreDataTest_3Tests
+//
+//  Created by Kim, Min Ho on 2016. 7. 15..
+//  Copyright © 2016년 Void Systems. All rights reserved.
+//
+
+import XCTest
+@testable import CoreDataTest_3
+
+class CoreDataTest_3Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
---
+++ PodTest/PodTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PodTest
+//
+//  Created by Kim, Min Ho on 2016. 8. 5..
+//  Copyright © 2016년 Void Systems. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
