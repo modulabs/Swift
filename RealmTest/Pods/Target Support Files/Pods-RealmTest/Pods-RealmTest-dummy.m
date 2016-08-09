b2217b0a602074b94d4ac08c2657dad80940a480
Min-ho Kim <xho1995@gmail.com>
2016-08-09 16:41:30 +0900

    Add: Realm Project

---
+++ RealmTest/Pods/Target Support Files/Pods-RealmTest/Pods-RealmTest-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RealmTest : NSObject
+@end
+@implementation PodsDummy_Pods_RealmTest
+@end
---
+++ RealmTest/Pods/Target Support Files/Pods-RealmTestTests/Pods-RealmTestTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RealmTestTests : NSObject
+@end
+@implementation PodsDummy_Pods_RealmTestTests
+@end
